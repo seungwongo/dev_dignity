463bc738fcc2f3bda01a5fa95b7822aa8643f020
Seungwon Go <seungwon.go@gmail.com>
2020-04-07 13:37:48 +0900

    var let const

---
+++ js-test/test/var-let-const.js
@@ -0,0 +1,17 @@
+// var a = "South Korea";
+// console.log(a);
+
+// var a = "North Korea";
+// console.log(a);
+
+// let a = "South Korea";
+// console.log(a);
+
+// a = "North Korea";
+// console.log(a);
+
+// const a = "South Korea";
+// console.log(a);
+
+// a = "North Korea";
+// console.log(a);
